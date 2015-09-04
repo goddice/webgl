1de16448da8f5b15ea635ec7119a4b12596e1860
goddice <liucl.lsu@gmail.com>
2015-09-04 00:30:06 -0500

    Added the vertex data and triangle data, now it can run normally

--- webgl/project1/prog1.js
+++ webgl/project1/prog1.js
@@ -10,8 +10,23 @@ function init()
 	program = createProgram(gl, vertexSource, fragmentSource);
 	gl.useProgram(program);
 
-	var positionBuffer = gl.createBuffer();
-	var triangleBuffer = gl.createBuffer();
+	positionBuffer = gl.createBuffer();
+	triangleBuffer = gl.createBuffer();
+
+	var positions = [
+		[0.0, 0.69282, 0.0],
+		[-0.4, 0.0, 0.0],
+		[0.4, 0.0, 0.0],
+		[-0.8, -0.69282, 0.0],
+		[0, -0.69282, 0.0],
+		[0.8, -0.69282, 0.0]
+	]
+
+	var triangles = [
+		[0, 1, 2],
+		[1, 3, 4],
+		[2, 4, 5]
+	]
 
 	positionArray = new Float32Array(flatten(positions));
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -20,17 +35,23 @@ function init()
 	triangleArray = new Uint16Array(flatten(triangles));
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleBuffer);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, triangleArray, gl.STATIC_DRAW);
+	draw();
 }
 
 function draw()
 {
-    gl.clearColor(0.0, 0.8, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.clearColor(0.0, 0.8, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 	var vertexPositionLocation = gl.getAttribLocation(program, 'vertexPosition');
 	gl.vertexAttribPointer(vertexPositionLocation, 3, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vertexPositionLocation);
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleBuffer);
 	gl.drawElements(gl.TRIANGLES, triangleArray.length, gl.UNSIGNED_SHORT, 0);
+}
+
+function flatten(a)
+{
+	return a.reduce(function (b, v) { b.push.apply(b, v); return b}, []);
 }
