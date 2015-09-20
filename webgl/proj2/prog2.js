f3a36f35be30f0b0a802f910b45fff9d7bc05050
goddice <liucl.lsu@gmail.com>
2015-09-20 04:37:19 -0500

    Rotation working

--- webgl/proj2/prog2.js
+++ webgl/proj2/prog2.js
@@ -1,6 +1,12 @@
 var canvas;
 var gl;
 
+var modelRotationX = 0;
+var modelRotationY = 0;
+var dragging = false;
+var lastClientX;
+var lastClientY;
+
 function init()
 {
 	canvas = document.getElementById('webgl');
@@ -14,15 +20,12 @@ function init()
 	triangleBuffer = gl.createBuffer();
 	colorBuffre = gl.createBuffer();
 
+    projectionMatrixLocation = gl.getUniformLocation(program, "projectionMatrix");
+    viewMatrixLocation = gl.getUniformLocation(program, "viewMatrix");
+    modelMatrixLocation = gl.getUniformLocation(program, "modelMatrix");
+
 	// x, y z coordinates of the vertices.
 	var positions = cube.vertices;
-	for (var i=0; i<positions.length; ++i)
-    {
-        for (var j=0; j<3; ++j)
-        {
-            positions[i][j] *= 0.9;
-        }
-    }
 
 	// vertex index
 	var triangles = cube.triangles;
@@ -41,6 +44,40 @@ function init()
 	colorArray = new Float32Array(flatten(colors));
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffre);
 	gl.bufferData(gl.ARRAY_BUFFER, colorArray, gl.STATIC_DRAW);
+
+    canvas.onmousedown = function(ev){
+        dragging = true;
+        lastClientX = ev.clientX;
+        lastClientY = ev.clientY;
+    };
+
+    canvas.onmouseup = function(ev){
+        dragging = false;
+    };
+
+    canvas.onmousemove = function(ev) {
+        if (dragging)
+        {
+            var dX = ev.clientX - lastClientX;
+            var dY = ev.clientY - lastClientY;
+
+            modelRotationY = modelRotationY + dX;
+            modelRotationX = modelRotationX + dY;
+
+            if (modelRotationX > 90.0)
+            {
+                modelRotationX = 90.0;
+            }
+            if (modelRotationX < -90.0)
+            {
+                modelRotationX = -90.0;
+            }
+        }
+
+        lastClientX = ev.clientX;
+        lastClientY = ev.clientY;
+        draw();
+    };
 	draw();
 }
 
@@ -48,6 +85,18 @@ function draw()
 {
 	gl.clearColor(0.0, 0.8, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var projectionMatrix = new Matrix4();
+    projectionMatrix.setPerspective(45, 1, 1, 10);
+    var viewMatrix = new Matrix4();
+    viewMatrix.translate(0, 0, -5);
+    var modelMatrix = new Matrix4();
+    modelMatrix.rotate(modelRotationX, 1, 0, 0);
+    modelMatrix.rotate(modelRotationY, 0, 1, 0);
+
+    gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix.elements);
+    gl.uniformMatrix4fv(viewMatrixLocation, false, viewMatrix.elements);
+    gl.uniformMatrix4fv(modelMatrixLocation, false, modelMatrix.elements);
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 	var vertexPositionLocation = gl.getAttribLocation(program, 'vertexPosition');
@@ -59,9 +108,11 @@ function draw()
     gl.vertexAttribPointer(vertexColorLocation, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vertexColorLocation);
 
+    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleBuffer);
 	gl.drawElements(gl.TRIANGLES, triangleArray.length, gl.UNSIGNED_SHORT, 0);
-
 
 }
 
