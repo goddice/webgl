2167ade4ebb7de7d6bd8f8cc2df6ad981395dcc8
goddice <liucl.lsu@gmail.com>
2015-09-27 14:02:47 -0500

    ripple code

--- webgl/experiments/render.js
+++ webgl/experiments/render.js
@@ -1,4 +1,15 @@
+var canvas;
+var gl;
+
 function init()
 {
+    canvas = document.getElementById('webgl');
+    gl = getWebGLContext(canvas, false);
+    var vertexSource = document.getElementById('vertexShader').text;
+    var fragmentSource = document.getElementById('fragmentShader').text;
+    program = createProgram(gl, vertexSource, fragmentSource);
+    gl.useProgram(program);
+
+    positionBuffer = gl.createBuffer();
 
 }
